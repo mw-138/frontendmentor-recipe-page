15338adefe1a95a2bf38450ae586bc381448568b
mw138 <mattheww138@gmail.com>
2024-07-19 18:06:26 +0100

    feat: completed project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        stone: {
+          100: "hsl(30, 54%, 90%)",
+          150: "hsl(30, 18%, 87%)",
+          600: "hsl(30, 10%, 34%)",
+          900: "hsl(24, 5%, 18%)",
+        },
+        brown: {
+          800: "hsl(14, 45%, 36%)",
+        },
+        rose: {
+          800: "hsl(332, 51%, 32%)",
+          50: "hsl(330, 100%, 98%)",
+        },
+        background: "hsl(30, 54%, 90%)",
+      },
     },
   },
   plugins: [],
